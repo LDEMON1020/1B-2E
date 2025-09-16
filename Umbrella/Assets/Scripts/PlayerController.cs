36f23305be49fc27e96ff1f9d6fbe957a8fb1865
LD_EMON1020 <LD_EMON1020@g.shingu.ac.kr>
2025-09-16 16:02:12 +0900

    [Feat] 점프맵 추가 + 달리기 기능 추가(달리기+달리면 화면 확대되는 이펙트)

--- Umbrella/Assets/Scripts/PlayerController.cs
+++ Umbrella/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,30 +6,66 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public float jumpPower = 5f;
     public float gravity = -9.81f;
+
+    public CinemachineVirtualCamera virtualCam;
+    public float rotationSpeed = 10f;
+    private CinemachinePOV pov;
     private CharacterController characterController;
     private Vector3 velocity;
     public bool isGrounded;
+    public CinemachineSwitcher cs;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        pov = virtualCam.GetCinemachineComponent<CinemachinePOV>();
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = characterController.isGrounded;
+        if (isGrounded)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(x, 0, z);
+        Vector3 camForward = virtualCam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = virtualCam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 move = (camForward * z + camRight * x).normalized;
+        if (!cs.usingFreelook)
         characterController.Move(move * speed * Time.deltaTime);
 
-        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        float cameraYaw = pov.m_HorizontalAxis.Value;
+        Quaternion targetRot = Quaternion.Euler(0f, cameraYaw, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             velocity.y = jumpPower;
         }
+
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 10f;
+            virtualCam.m_Lens.FieldOfView = 80f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 5f;
+            virtualCam.m_Lens.FieldOfView = 60f;
+        }
     }
 }
